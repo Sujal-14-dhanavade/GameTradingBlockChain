573e3a84c25e560ad1da1200f00ee6deac1e7ed2
surojeet ghosh <surojeetghosh89@gmail.com>
2023-04-11 18:34:08 +0530

    added logs in withdraw

--- scripts/withdraw/withdraw.js
+++ scripts/withdraw/withdraw.js
@@ -1,14 +1,54 @@
 // withdrawing money from tokens
-const {getNamedAccounts, ethers} = require("hardhat");
+const { getNamedAccounts, ethers } = require("hardhat");
 
 async function main() {
-    const {deployer, actor} = await getNamedAccounts();
+    console.log(
+        "------------------------------------------------------------------------"
+    );
+    console.log(
+        "------------------------------------------------------------------------"
+    );
+    console.log("Deployer withdrawing money from token contract");
+    console.log("\n");
+
+    const { deployer } = await getNamedAccounts();
+    const provider = ethers.provider;
+
+    console.log(
+        "\t\tDeployer address : - " + deployer + " accessing Token contract"
+    );
+    console.log(
+        "\t\t\t* Deployer's Balance before withdraw : - " +
+            hre.ethers.utils.formatEther(
+                // getBalance returns wei amount, format to ETH amount
+                await provider.getBalance(deployer)
+            ) +
+            " ethers"
+    );
+
     const token = await ethers.getContract("Token", deployer);
+
+    console.log("\t\t\t* Deployer withdrawing money from token contract");
+
     const response = await token.withDraw();
     await response.wait(1);
+
+    console.log(
+        "\t\t\t* Deployer's Balance after withdraw : - " +
+            hre.ethers.utils.formatEther(
+                // getBalance returns wei amount, format to ETH amount
+                await provider.getBalance(deployer)
+            ) +
+            " ethers"
+    );
+    console.log(
+        "------------------------------------------------------------------------"
+    );
+    console.log(
+        "------------------------------------------------------------------------"
+    );
 }
 
-
-main().catch(err => {
+main().catch((err) => {
     console.log(err);
-})
+});
